7b3f750ae9d21ea47c9250ce51dea62597285d67
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-03-01 11:58:04 -0500

    wip: adds a database connection working

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+
+const mongoose = require('mongoose');
+const express = require('express');
+
+// Aplication object
+const app = express();
+
+// Aplication settings
+require('dotenv').config();
+
+const { PORT = 5001, DATABASE_URL } = process.env;
+
+// Database connection
+mongoose.connect(DATABASE_URL);
+
+//Mongoose conection events
+mongoose.connection
+.on('open', () => console.log('you are connected to MongoDB'))
+.on('close', () => console.log('You are disconnected from MongoDB'))
+.on('error', () => console.log(`MongoDB Error: ${error.message}`)) 
+
+app.listen(PORT, () => console.log(`Express is listening on port; ${PORT}`));
+
+
