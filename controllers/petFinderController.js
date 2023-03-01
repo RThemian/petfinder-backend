598226768cedac1298d7aa21edf45ec60b0ee542
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-03-01 13:44:43 -0500

    wip: requires models and create async function to call the create route

---
+++ controllers/petFinderController.js
@@ -0,0 +1,22 @@
+const PetFinder = require ('../models/petFinder');
+
+const getPet = async (req, res) => {
+    try {
+        res.status(200).json(await PetFinder.find({}));
+    } catch (error) {
+        res.status(400).json({ message: 'Something went wrong'});
+    };
+};
+
+const createPet = async (req, res) => {
+    try {
+        res.status(201).json(await PetFinder.create(req.body));
+    } catch (error) {
+        res.status(400).json({ message: 'Something went wrong'});
+    };
+};
+
+module.exports = {
+    getPet,
+    createPet
+};
