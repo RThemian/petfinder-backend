d935627c5ba999a54fd428d3f542bb00bf5cf028
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-03-01 13:41:49 -0500

    wip: adds Schema model

---
+++ models/petFinder.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const PetSchema = new mongoose.Schema({
+	animals: {
+    name: String,
+	photo: String,
+    age: String,
+    description:String,
+	species: String,
+    breeds: String,
+    gender: String,
+    videos: String,
+    status: String, 
+},
+    contact: {
+    email: String,
+    phone: String,
+    address: {
+        address1: String,
+        city: String,
+        state: String,
+        postcode: String,
+        country: String
+    },
+   }});
+
+module.exports = mongoose.model('petFinder', PetSchema);
